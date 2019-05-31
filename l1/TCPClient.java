79dbc0fef361f07e90cfdc2433dfb719d5b77a71
AnnyCaroline <annycarolinegnr@gmail.com>
2019-05-31 01:12:55 -0300

    out - txt and csv

--- l1/TCPClient.java
+++ l1/TCPClient.java
@@ -3,6 +3,8 @@ import java.net.*;
 
 public class TCPClient
 {
+   private static boolean txt = false; //false:csv true:txt
+
    public static void main(String args[]) throws Exception{
 
       String host=host = args[0];
@@ -14,15 +16,28 @@ public class TCPClient
       DataInputStream in=new DataInputStream(socket.getInputStream());  
       DataOutputStream out=new DataOutputStream(socket.getOutputStream());
 
-      String l1 = String.format("%-12s ", "TCP/2/100");
-      for (int k=1, j=0; j<=16; j++, k=k*2){
-         l1 = l1 + String.format("%-12d ", k);
+      String l1;
+      String lMed;
+      String lDvp;
+      if (txt){
+         l1 = String.format("%-12s ", "TCP/2/100");
+         for (int k=1, j=0; j<=16; j++, k=k*2){
+            l1 = l1 + String.format("%-12d ", k);
+         }
+
+         lMed = String.format("%-12s ", "MED");
+         lDvp = String.format("%-12s ", "DVP");         
+      }else{
+         l1 = new String();
+         for (int k=1, j=0; j<=16; j++, k=k*2){
+            l1 = l1 + String.format("%d;", k);
+         }
+
+         lMed = new String();
+         lDvp = new String();
       }
-      String lMed = String.format("%-12s ", "MED");
-      String lDvp = String.format("%-12s ", "DVP");
 
       for (int k=1, j=0; j<=16; j++, k=k*2){
-         
          long rtt[] = new long[iterations];
          double media = 0;
 		   double desvioPadrao = 0;
@@ -54,21 +69,26 @@ public class TCPClient
          for (int i=10; i<iterations; i++)
             media += rtt[i];
          media = ((double)media)/(iterations-10);
-         lMed = lMed + String.format("%-12.2f ", media);
+         if (txt) 
+            lMed = lMed + String.format("%-12.2f ", media);
+         else
+            lMed = lMed + String.format("%.2f;", media);
 
          // Desvio Padrao - descartando as 10 primeiras medidas
          for (int i=10; i<iterations; i++)
             desvioPadrao += Math.pow(rtt[i] - media,2);
          desvioPadrao = desvioPadrao/(iterations-10);
          desvioPadrao = Math.sqrt(desvioPadrao);
-         lDvp = lDvp + String.format("%-12.2f ", desvioPadrao);
+         if (txt) 
+            lDvp = lDvp + String.format("%-12.2f ", desvioPadrao);
+         else
+            lDvp = lDvp + String.format("%.2f;", desvioPadrao);
 
          //System.out.println("["+k+", "+media+", "+desvioPadrao+"]");
       }
 
       socket.close(); // fecha socket
 
-      
       System.out.println(l1);
       System.out.println(lMed);
       System.out.println(lDvp);
